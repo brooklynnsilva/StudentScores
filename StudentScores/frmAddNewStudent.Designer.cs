5149b1debe149e2d4c004e1c038cdc3154e976bd
brooklynnsilva <brooklynnsilva@gmail.com>
2023-11-08 20:32:27 -0700

    Add New Student form complete

--- StudentScores/frmAddNewStudent.Designer.cs
+++ StudentScores/frmAddNewStudent.Designer.cs
@@ -28,19 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            txtName = new TextBox();
+            label2 = new Label();
+            txtScore = new TextBox();
+            label3 = new Label();
+            lblScores = new Label();
+            btnAdd = new Button();
+            btnClear = new Button();
+            btnCancel = new Button();
+            btnOK = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(8, 15);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Name:";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(52, 12);
+            txtName.Margin = new Padding(4, 3, 4, 3);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(256, 23);
+            txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 56);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Score:";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // txtScore
+            // 
+            txtScore.Location = new Point(52, 53);
+            txtScore.Margin = new Padding(4, 3, 4, 3);
+            txtScore.Name = "txtScore";
+            txtScore.Size = new Size(150, 23);
+            txtScore.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 100);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Scores:";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblScores
+            // 
+            lblScores.BorderStyle = BorderStyle.Fixed3D;
+            lblScores.Location = new Point(52, 94);
+            lblScores.Margin = new Padding(2, 0, 2, 0);
+            lblScores.Name = "lblScores";
+            lblScores.Size = new Size(254, 27);
+            lblScores.TabIndex = 0;
+            lblScores.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(220, 50);
+            btnAdd.Margin = new Padding(4, 3, 4, 3);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(88, 27);
+            btnAdd.TabIndex = 3;
+            btnAdd.Text = "&Add Score";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(218, 134);
+            btnClear.Margin = new Padding(4, 3, 4, 3);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(88, 27);
+            btnClear.TabIndex = 4;
+            btnClear.Text = "Clear &Scores";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(218, 184);
+            btnCancel.Margin = new Padding(4, 3, 4, 3);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(88, 27);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "&Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            btnOK.DialogResult = DialogResult.OK;
+            btnOK.Location = new Point(122, 184);
+            btnOK.Margin = new Padding(4, 3, 4, 3);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(88, 27);
+            btnOK.TabIndex = 6;
+            btnOK.Text = "&OK";
+            btnOK.UseVisualStyleBackColor = true;
             // 
             // frmAddNewStudent
             // 
+            AcceptButton = btnOK;
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(300, 179);
+            CancelButton = btnCancel;
+            ClientSize = new Size(340, 225);
+            Controls.Add(btnOK);
+            Controls.Add(btnCancel);
+            Controls.Add(btnClear);
+            Controls.Add(btnAdd);
+            Controls.Add(lblScores);
+            Controls.Add(label3);
+            Controls.Add(txtScore);
+            Controls.Add(label2);
+            Controls.Add(txtName);
+            Controls.Add(label1);
             Name = "frmAddNewStudent";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Add New Student";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtName;
+        private Label label2;
+        private TextBox txtScore;
+        private Label label3;
+        private Label lblScores;
+        private Button btnAdd;
+        private Button btnClear;
+        private Button btnCancel;
+        private Button btnOK;
     }
 }
